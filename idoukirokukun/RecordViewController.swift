a6679d9c7b7eff55d471f338d76d9aa83e6d441a
Takuroh Shimizu <his7716twd@gmail.com>
2020-06-19 01:14:28 +0900

    基本的な部分は完成

---
+++ idoukirokukun/RecordViewController.swift
@@ -0,0 +1,142 @@
+//
+//  RecordViewController.swift
+//  idoukirokukun
+//
+//  Created by 清水拓郎 on 2020/06/19.
+//  Copyright © 2020 清水拓郎. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class RecordViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+    
+    @IBOutlet var mapView: MKMapView!
+    var locManager: CLLocationManager!
+    var coordinateArray: [CLLocationCoordinate2D] = []
+    var colorNumber = 0
+    var colorNumberArray: [Int] = []
+    @IBOutlet var label : UILabel!
+    //var lon: Double = 0
+    //var lat: Double = 0
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        locManager = CLLocationManager()
+        locManager.delegate = self
+        mapView.delegate = self
+        
+        initMap()
+        
+        //self.start()
+        locManager.startUpdatingLocation()
+        locManager.distanceFilter = 5
+        if colorNumber == 0 {
+            label.text = "選択中の色：青"
+        } else if colorNumber == 1 {
+            label.text = "選択中の色：赤"
+        } else if colorNumber == 2 {
+            label.text = "選択中の色：緑"
+        } else if colorNumber == 3 {
+            label.text = "選択中の色：グレー"
+        }
+        
+       
+    }
+    
+    
+   
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations:[CLLocation]) {
+        //決められた間隔ごとに現在地を取得
+        let coordinateNow = (locations.last?.coordinate)!
+        
+        //それぞれ配列に追加していく
+        coordinateArray.append(coordinateNow)
+        
+        //配列の現在の個数をarrayNumberで表す
+        let arrayNumber = coordinateArray.count
+        
+        
+        print(coordinateArray)
+        
+        
+        //現在の座標データをcoordinate1に、一個前の座標データをcoordinate2にする
+        if arrayNumber > 1 {
+            let coordinate1 = coordinateArray[arrayNumber - 1]
+            let coordinate2 = coordinateArray[arrayNumber - 2]
+            //polylineを引くcoordinatesを設定する。
+            let coordinates = [coordinate1, coordinate2]
+            let PolyLine: MKPolyline = MKPolyline(coordinates: coordinates, count: coordinates.count)
+            DispatchQueue.main.async {
+                self.mapView.addOverlay(PolyLine)
+            }
+            mapView.userTrackingMode = .follow
+            
+        }
+    }
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        if let polyline = overlay as? MKPolyline {
+            let polylineRenderer = MKPolylineRenderer(polyline: polyline)
+            if colorNumber == 0 {
+                polylineRenderer.strokeColor = .blue
+                colorNumberArray.append(0)
+            } else if colorNumber == 1 {
+                polylineRenderer.strokeColor = .red
+                colorNumberArray.append(1)
+            } else if colorNumber == 2 {
+                polylineRenderer.strokeColor = .green
+                colorNumberArray.append(2)
+            } else if colorNumber == 3 {
+                polylineRenderer.strokeColor = .gray
+                colorNumberArray.append(3)
+            }
+            print(colorNumberArray)
+            polylineRenderer.lineWidth = 7.0
+            return polylineRenderer
+        }
+        return MKOverlayRenderer()
+    }
+    
+    
+    
+    
+    func initMap() {
+    var region:MKCoordinateRegion = mapView.region
+    region.span.latitudeDelta = 0.1
+    region.span.longitudeDelta = 0.1
+    mapView.setRegion(region, animated: true)
+    
+    //現在位置表示の有効化
+    mapView.showsUserLocation = true
+    //現在位置設定（デバイスの動きとしてこの時の一回だけ中心位置が現在位置で更新される
+    mapView.userTrackingMode = .follow
+    
+    }
+    
+    @IBAction func blueButtonTapped() {
+        colorNumber = 0
+        label.text = "選択中の色：青"
+    }
+    @IBAction func redButtonTapped() {
+        colorNumber = 1
+        label.text = "選択中の色：赤"
+    }
+    @IBAction func greenButtonTapped() {
+        colorNumber = 2
+        label.text = "選択中の色：緑"
+    }
+    @IBAction func grayButtonTapped() {
+        colorNumber = 3
+        label.text = "選択中の色：グレー"
+    }
+    @IBAction func stopUpdating() {
+        locManager.stopUpdatingLocation()
+        mapView.showsUserLocation = false
+    }
+}
---
+++ idoukirokukun/StartViewController.swift
@@ -0,0 +1,97 @@
+//
+//  StartViewController.swift
+//  idoukirokukun
+//
+//  Created by 清水拓郎 on 2020/06/19.
+//  Copyright © 2020 清水拓郎. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class StartViewController: UIViewController, CLLocationManagerDelegate {
+    
+    
+    @IBOutlet var mapView: MKMapView!
+    var locManager: CLLocationManager!
+    var colorNumber = 0
+    @IBOutlet var label: UILabel!
+    
+   
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        locManager = CLLocationManager()
+        locManager.delegate = self
+        
+        //位置情報の使用の許可を得る
+        locManager.requestWhenInUseAuthorization()
+        if CLLocationManager.locationServicesEnabled(){
+            switch CLLocationManager.authorizationStatus() {
+            case .authorizedWhenInUse:
+                //座標の表示
+                locManager.startUpdatingLocation()
+                break
+            default:
+                break
+           
+            }
+            mapView.showsUserLocation = true
+        }
+        initMap()
+       
+        
+        
+    }
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations:[CLLocation]) {
+    }
+    
+    func initMap() {
+        var region:MKCoordinateRegion = mapView.region
+        region.span.latitudeDelta = 0.1
+        region.span.longitudeDelta = 0.1
+        mapView.setRegion(region, animated: true)
+        
+        //現在位置表示の有効化
+        mapView.showsUserLocation = true
+        //現在位置設定（デバイスの動きとしてこの時の一回だけ中心位置が現在位置で更新される
+        mapView.userTrackingMode = .follow
+    }
+    
+    @IBAction func back(sender: UIStoryboardSegue) {
+        
+    }
+    @IBAction func blueButtonTapped() {
+        colorNumber = 0
+        label.text = "選択中の色：青"
+    }
+    @IBAction func redButtonTapped() {
+        colorNumber = 1
+        label.text = "選択中の色：赤"
+    }
+    @IBAction func greenButtonTapped() {
+        colorNumber = 2
+        label.text = "選択中の色：緑"
+    }
+    @IBAction func grayButtonTapped() {
+        colorNumber = 3
+        label.text = "選択中の色：グレー"
+    }
+    @IBAction func startButtonTapped() {
+        performSegueToRecord()
+    }
+    func performSegueToRecord() {
+        performSegue(withIdentifier: "toRecordView", sender: nil)
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toRecordView" {
+            let recordViewController = segue.destination as! RecordViewController
+            recordViewController.colorNumber = self.colorNumber
+        }
+    }
+
+
+}
+
