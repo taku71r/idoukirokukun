03d1b193a40b5053815afdf708da7bbe09f03d25
Takuroh Shimizu <his7716twd@gmail.com>
2020-06-19 00:14:56 +0900

    Initial Commit

---
+++ idoukirokukun/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  idoukirokukun
+//
+//  Created by 清水拓郎 on 2020/06/19.
+//  Copyright © 2020 清水拓郎. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
